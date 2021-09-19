c27253d080aa87997572dbb419a822dbf574ac26
Helgurd <rekauta@gmail.com>
2021-09-19 16:41:10 +0000

    dom loaded content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+document.addEventListener('DOMContentLoaded', function(){
+    let buttons= document.getElementsByTagName('button');
+})
+
 function runGame(){
 
 }
