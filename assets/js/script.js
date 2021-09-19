ccdbb51840eb0da0f87bb23d0dfe9b509c1e6065
Helgurd <rekauta@gmail.com>
2021-09-19 20:45:56 +0000

    some funciton added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,10 @@ document.addEventListener('DOMContentLoaded', function(){
     let buttons= document.getElementsByTagName('button');
     for(let button of buttons){
         button.addEventListener('click', function(){
-            
+            if(this.getAttribute('data-type') === 'submit'){
+                alert('You clicked Submit');
+            }
+
          })
     }
 })
