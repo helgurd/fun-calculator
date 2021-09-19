230d39485bcad144212c1da99e0fdaaf1c90b5f9
Helgurd <rekauta@gmail.com>
2021-09-19 23:41:33 +0000

    funtions and dataTypes added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,49 @@
-
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
-document.addEventListener('DOMContentLoaded', function(){
-    let buttons= document.getElementsByTagName('button');
-    for(let button of buttons){
-        button.addEventListener('click', function(){
-            if(this.getAttribute('data-type') === 'submit'){
-                alert('You clicked Submit');
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
             }
-
-         })
+        });
     }
-})
+});
 
-function runGame(){
-
-}
-
-function checkAnswer(){
+function runGame() {
 
 }
 
-
-function calculateCorrectAnswer(){
-
-}
-
-
-function incrementScore(){
+function checkAnswer() {
 
 }
 
-function incrementWrongAnswer(){
+function calculateCorrectAnswer() {
 
 }
 
-function displayAdditionQustion(){
+function incrementScore() {
 
 }
 
-function displaySubtractQuestion(){
+function incrementWrongAnswer() {
 
 }
 
-function displayMultiplyQuestion(){
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
     
 }
